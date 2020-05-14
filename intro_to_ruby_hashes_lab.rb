1ec1d1aa19dcd7948d16ca05a1496b20f1a91b20
Zachary Lynch <zachary.lynch1337@gmail.com>
2020-05-14 18:14:21 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -33,9 +33,7 @@ end
 
 def monopoly_with_fourth_tier
 	monopoly = {}
-	monopoly[:railroads][:pieces] = 4
 	monopoly[:railroads][:rent_in_dollars] = {}
-	monopoly[:railroads][:names] = {}
 	monopoly
 
 end
